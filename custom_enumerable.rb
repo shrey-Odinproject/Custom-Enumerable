d956410ee211eb23e20e18170781258f3d26ea4d
shrey-Odinproject <sonishreyf1@gmail.com>
2022-01-06 00:02:13 +0530

    created my_each_with_index method

--- custom_enumerable.rb
+++ custom_enumerable.rb
@@ -9,9 +9,23 @@ module Enumerable
     end
     self
   end
+  def my_each_with_index
+    return 'no block given' unless block_given?
+
+    i = 0
+    while i != self.length
+      yield(self[i], i)
+      i += 1
+    end
+    self
+  end
 end
 
 puts "my_each vs. each"
 numbers = [1, 2, 3, 4, 5]
-numbers.my_each  { |item| puts item }
-numbers.each { |item| puts item }
+
+#numbers.my_each  { |item| puts item }
+#numbers.each { |item| puts item }
+
+#numbers.each_with_index {|item, idx| puts "#{idx} #{item}"}
+numbers.my_each_with_index {|item, idx| puts "#{idx} -> #{item}"}
