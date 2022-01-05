b5359f1f0c2e3c1caa4c494f84f3a58301ddd892
shrey-Odinproject <sonishreyf1@gmail.com>
2022-01-05 17:36:50 +0530

    created my_each & got it to work

--- custom_enumerable.rb
+++ custom_enumerable.rb
@@ -1,10 +1,11 @@
 module Enumerable
   def my_each
-    
+    yield self
+    self
   end
 end
 
 puts "my_each vs. each"
 numbers = [1, 2, 3, 4, 5]
 numbers.my_each  { |item| puts item }
-numbers.each  { |item| puts item }
+numbers.each { |item| puts item }
