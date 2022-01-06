4a99beca66cd6a7aeeb9b909a83fa500e28a240a
shrey-Odinproject <sonishreyf1@gmail.com>
2022-01-06 21:07:12 +0530

    created my_map method using my_each , started my_inject's creation

--- custom_enumerable.rb
+++ custom_enumerable.rb
@@ -1,3 +1,24 @@
+# numbers.my_each  { |item| puts item }
+# numbers.each { |item| puts item }
+
+# numbers.each_with_index {|item, idx| puts "#{idx} #{item}"}
+# numbers.my_each_with_index {|item, idx| puts "#{idx} -> #{item}"}
+
+# p numbers.select { |item|  puts item.even? }
+# p numbers.my_select { |item| puts item.even? }
+
+# numbers.all? { |item| item > 0 }
+# numbers.my_all? { |item| item > 0 }
+
+# p numbers.any? {|item| item >9}
+# p numbers.my_any? {|item| item >9}
+
+# p numbers.none? { |item| item > 110 }
+# p numbers.my_none? { |item| item > 110 }
+
+# p numbers.count(0)
+# p numbers.my_count(0)
+
 module Enumerable
   def my_each
     return 'no block given' unless block_given?
@@ -65,27 +86,26 @@ module Enumerable
     end
     counter
   end
+
+  def my_map
+    formatted = []
+    self.my_each do |item|
+      formatted.push(yield(item))
+    end
+    formatted
+  end
+
+  def my_inject(accm = 0)
+    self.my_each do |item|
+      yield(accm, item)
+    end
+  end
 end
 
 numbers = [1, 2, 3, 4, 5]
 
-# numbers.my_each  { |item| puts item }
-# numbers.each { |item| puts item }
+p numbers.map { |item| item**2 }
+p numbers.my_map { |item| item**2 }
 
-# numbers.each_with_index {|item, idx| puts "#{idx} #{item}"}
-# numbers.my_each_with_index {|item, idx| puts "#{idx} -> #{item}"}
-
-# p numbers.select { |item|  puts item.even? }
-# p numbers.my_select { |item| puts item.even? }
-
-# numbers.all? { |item| item > 0 }
-# numbers.my_all? { |item| item > 0 }
-
-# p numbers.any? {|item| item >9}
-# p numbers.my_any? {|item| item >9}
-
-# p numbers.none? { |item| item > 110 }
-# p numbers.my_none? { |item| item > 110 }
-
-p numbers.count(0)
-p numbers.my_count(0)
+# p numbers.inject {|result, elm| result - elm }
+# p numbers.my_inject { |result, elm| result - elm }
